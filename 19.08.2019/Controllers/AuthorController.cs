a9de30ec842da57c06f7c4e5818ef0e1277574fd
ЫбрайА <@DESKTOP-4VVLG99>
2019-08-21 20:52:06 +0600

    adding create edit auth and books

--- 19.08.2019/Controllers/AuthorController.cs
+++ 19.08.2019/Controllers/AuthorController.cs
@@ -4,22 +4,67 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace _19._08._2019.Controllers
+namespace _19._08._2019
 {
     public class AuthorController : Controller
     {
-        
         // GET: Author
-
         public ActionResult Index()
         {
-            List<Authors> list = new List<Authors>();
-            using(Model1 db =new Model1())
+            List<Authors> authors;
+            using (Model1 db = new Model1())
             {
-                list = db.Authors.ToList();
-                //comment
+                authors = db.Authors.ToList();
             }
-            return View(list);
+
+            return View(authors);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Authors author)
+        {
+
+            using (Model1 db = new Model1())
+            {
+                db.Authors.Add(author);
+                db.SaveChanges();
+            }
+
+            return Redirect("Index");
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            Authors author;
+            using (Model1 db = new Model1())
+            {
+                author = db.Authors.Where(x => x.Id == id).FirstOrDefault();
+            }
+            return View(author);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Authors author)
+        {
+
+            using (Model1 db = new Model1())
+            {
+                var oldAuthor = db.Authors.Where(x => x.Id == author.Id).FirstOrDefault();
+                oldAuthor.FirstName = author.FirstName;
+                oldAuthor.LastName = author.LastName;
+                db.SaveChanges();
+            }
+
+            return RedirectToActionPermanent("Index", "Author");
+        }
+
     }
 }
---
+++ 19.08.2019/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _19._08._2019
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            List<Books> books;
+            using (Model1 db = new Model1())
+            {
+
+                books = db.Books.ToList();
+            }
+
+            return View(books);
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Books book)
+        {
+            using (Model1 db = new Model1())
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+
+            }
+            return RedirectToActionPermanent("Index", "Book");
+        }
+    }
+}
