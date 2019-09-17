be5bb28321d156c53f83ad1373547a28220b918c
Аман Ыбрай <ybray1986@gmail.com>
2019-09-17 16:49:20 +0600

    automapper and ninject binded

---
+++ BusinessLayer/Profiles/BLProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BusinessLayer.DataTransferObjects;
+using DataLayer.Entities;
+
+namespace BusinessLayer.Profiles
+{
+    public class BLProfile: Profile
+    {
+        public BLProfile()
+        {
+            this.CreateMap<Authors, AuthorsDTO>().ReverseMap();
+        }
+    }
+}
